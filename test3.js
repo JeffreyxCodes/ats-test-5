8ccfa1617e046371b4e54a7b0804e94b917d5b74
jeffreyxdc <jeffreyxdc@gmail.com>
2019-07-03 15:01:39 -0400

    test3 done

--- test3.js
+++ test3.js
@@ -5,7 +5,14 @@ const Immutable = require('immutable');
 // e.g. in the fromShape, we have cat and snake with 'long' == true
 // so in the toShape, we want long: [ 'cat', 'snake' ]
 const transform = (fromShape) => {
-  return fromShape;
+  const animals = fromShape.keySeq();
+  const traits = fromShape.get(animals.get('0')).keySeq();
+
+  return Immutable.Map().withMutations(mutable => {
+    traits.forEach(trait => {
+      mutable.set(trait, animals.filter(animal => fromShape.get(animal).get(trait)));
+    })
+  });
 };
 
 const fromShape = Immutable.fromJS({
