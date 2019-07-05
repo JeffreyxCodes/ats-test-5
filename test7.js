83ad6d2f8a0539a737985c8b9a929a8dcff9608f
jeffreyxdc <jeffreyxdc@gmail.com>
2019-07-04 20:51:58 -0400

    test7 done

--- test7.js
+++ test7.js
@@ -2,7 +2,23 @@ const Immutable = require('immutable');
 
 // similar to test6, but group the elements by what they would be mapped to in fizzbuzz 
 const transform = (fromShape) => {
-  return fromShape;
+  return Immutable.Map().withMutations(mutable => {
+    mutable.set('fizz', Immutable.List())
+      .set('buzz', Immutable.List())
+      .set('fizzbuzz', Immutable.List())
+      .set('other', Immutable.List());
+    fromShape.forEach(num => {
+      if (num % 15 === 0) {
+        mutable.setIn(['fizzbuzz', mutable.get('fizzbuzz').size], num);
+      } else if (num % 3 === 0) {
+        mutable.setIn(['fizz', mutable.get('fizz').size], num);
+      } else if (num % 5 === 0) {
+        mutable.setIn(['buzz', mutable.get('buzz').size], num);
+      } else {
+        mutable.setIn(['other', mutable.get('other').size], num);
+      }
+    });
+  });
 };
 
 const fromShape = Immutable.fromJS([
