7e20c67b9df69bac73bb33795e1dfabecb010c70
jeffreyxdc <jeffreyxdc@gmail.com>
2019-07-04 20:41:42 -0400

    test6 done and included test6, test7, test8 in index.js

--- test6.js
+++ test6.js
@@ -2,7 +2,17 @@ const Immutable = require('immutable');
 
 // map 'fizzbuzz' if multiple of three AND five, 'fizz' if multiple of three, 'buzz' if multiple of five, or just the number else
 const transform = (fromShape) => {
-  return fromShape;
+  return fromShape.withMutations(mutable => {
+    mutable.forEach((num, index) => {
+      if (num % 15 === 0) {
+        mutable.set(index, 'fizzbuzz');
+      } else if (num % 3 === 0) {
+        mutable.set(index, 'fizz');
+      } else if (num % 5 === 0) {
+        mutable.set(index, 'buzz');
+      }
+    });
+  });
 };
 
 const fromShape = Immutable.fromJS([
